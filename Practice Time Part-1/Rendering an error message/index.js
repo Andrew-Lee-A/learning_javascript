4011144665458e87f718f83e5317f0302970577c
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-04-18 19:33:55 +1200

    Completed Practice 1

---
+++ Practice Time Part-1/Rendering an error message/index.js
@@ -0,0 +1,9 @@
+let errorParagraph = document.getElementById("error");
+console.log(errorParagraph);
+
+function error(){
+    errorParagraph.innerText = "Something went wrong";
+    console.log("error clicked");
+}
+
+
